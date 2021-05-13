10af79ac1ee8f6f410e21f3d5e1a9761bf650ef5
truth1984 <zhengyao365@163.com>
2021-05-13 15:10:50 +0800

    three way link

--- bin/help.js
+++ bin/help.js
@@ -7,11 +7,64 @@ module.exports = class {
     this.mainInfo = mainInfo;
     this.entryInfo = {};
     this.argsInfo = {};
+    this.perform = {};
   }
 
   addEntry(name, description = "", argsMap = {}) {
     this.entryInfo[name] = description;
     this.argsInfo[name] = argsMap;
+
+    this._holder = { name };
+    return this;
+  }
+
+  /**
+   * @typedef {{origin:{argv:{}},argv:{},entry?:string, _?:{}, args?:{}, kwargs?:{}}} store
+   * @param {{_?:int, args?:string, kwargs?:string}[]} listen _ for index position; bind _, args and kwargs together
+   *
+   */
+  addLink(listen = [{}]) {
+    /**
+     * @param {store} storage
+     * three way bind
+     */
+    let sequence = (storage) => {
+      let freshObj = { _: [], args: {}, kwargs: {} };
+      for (let i in storage._) {
+        freshObj._[i] = storage._[i];
+        u.arrayOfMapSearch(listen, { _: i }).map((item) => {
+          if (item.args != undefined) freshObj.args[item.args] = [storage._[i]];
+          if (item.kwargs != undefined) freshObj.kwargs[item.kwargs] = [storage._[i]];
+        });
+      }
+
+      for (let i in storage.args) {
+        freshObj.args[i] = storage.args[i];
+        u.arrayOfMapSearch(listen, { args: i }).map((item) => {
+          if (item._ != undefined) freshObj._[item._] = storage.args[i];
+          if (item.kwargs != undefined) freshObj.kwargs[item.kwargs] = storage.args[i];
+        });
+      }
+
+      for (let i in storage.kwargs) {
+        freshObj.kwargs[i] = storage.kwargs[i];
+        u.arrayOfMapSearch(listen, { kwargs: i }).map((item) => {
+          if (item._ != undefined) freshObj._[item._] = storage.kwargs[i];
+          if (item.args != undefined) freshObj.args[item.args] = storage.kwargs[i];
+        });
+      }
+
+      return freshObj;
+    };
+    this._holder.sequence = sequence;
+    return this;
+  }
+
+  /**
+   * @param {(storage:store)=>{}} action
+   */
+  addAction(action = () => {}) {
+    this.perform[this._holder.name] = (storage) => action(u.mapMerge(storage, this._holder.sequence(storage)));
   }
 
   compile() {
@@ -25,5 +78,9 @@ module.exports = class {
       for (let [k, l] of u.mapEntries(this.argsInfo[i])) result[i] += k + getSpace(k) + l + "\n";
     }
     return result;
+  }
+
+  compileExecute() {
+    return this.perform;
   }
 };
--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 const yargslite = require("yargs-lite");
-const Help = require("./help");
-let y = new yargslite();
+const Helpdoc = require("./help");
+const u = require("awadau");
 
-let help = new Help("u", "self managed scripts");
+let yargs = new yargslite();
+let h = new Helpdoc("u", "Author: Awada.Z");
 
-help.addEntry("display", "display the storage", { "-d": "only display -d value" });
+h.addEntry("display", "display the storage", { "-d": "only display -d value" })
+  .addLink([{ _: 0, args: "d", kwargs: "dis" }])
+  .addAction((argv) => console.log(argv));
 
-y.help.helpDoc = help.compile();
+h.addEntry("echo", "echo the command", { "-d": "only display -d value" })
+  .addLink([{ _: 0, args: "l", kwargs: "line" }])
+  .addAction((argv) => console.log(argv.kwargs.line));
 
-y.run();
+yargs.help.helpDoc = h.compile();
+yargs._execution = h.compileExecute();
+yargs.run();
