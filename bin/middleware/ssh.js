22fdd4fa940ce00ed89eb79d8e3a6826f2ea4b74
truth1984 <zhengyao365@163.com>
2021-05-31 10:20:23 +0800

    fix command

--- bin/middleware/ssh.js
+++ bin/middleware/ssh.js
@@ -33,7 +33,7 @@ h.addEntry("ssh", "use keygen to generate key pairs", {
       let status = cmd(`ssh-copy-id -i ~/.ssh/id_rsa.pub -p ${port} ${user}@${addr}`, 0, 1, 1).status;
       if (status > 0) return cu.cmderr("ssh-copy-id status > 0, abort", "ssh");
       cmd(`u quick ${name} -c "ssh -p ${port} ${user}@${addr}"`);
-      cmd(`u ansible -i ${addr} ${name} '${desc}'`);
+      cmd(`u ansible -a ${addr} ${name} '${desc}'`);
     }
 
     if (refresh) return cmd("ssh-keygen -t rsa -b 4096");
