7f3b9c6ed51a594ec623b35d0e105333536b610f
truth1984 <zhengyao365@163.com>
2021-06-08 17:21:50 +0800

    add generate string for proxy

--- bin/middleware/proxy.js
+++ bin/middleware/proxy.js
@@ -1,14 +1,25 @@
 const { h, cmd, u } = require("../head");
-h.addEntry("proxy", "proxy the command", { "-c,--command": "command to use proxy" })
-  .addLink({ _: 0, args: "c", kwargs: "command" })
+h.addEntry("proxy", "proxy the command", {
+  "[0],-c,--command": "command to use proxy",
+  "-g,--generate": "generate proxy string",
+})
+  .addLink({ _: 0, args: "c", kwargs: "command" }, { args: "g", kwargs: "generate" })
   .addAction((argv) => {
     let args = argv.args;
     let command = args.c;
+    let generate = args.g;
 
     let proxystr = "";
     let env = process.env;
     let checkKey = ["https_proxy", "http_proxy", "HTTPS_PROXY", "HTTP_PROXY", "no_proxy", "NO_PROXY"];
-    for (let i of checkKey) proxystr += env[i] ? i + "=" + env[i] + " " : "";
 
+    if (generate) {
+      let url = generate[0];
+      return console.log(
+        `export https_proxy=${url}\nexport http_proxy=${url}\nexport HTTPS_PROXY=${url}\nexport HTTP_PROXY=${url}`
+      );
+    }
+
+    for (let i of checkKey) proxystr += env[i] ? i + "=" + env[i] + " " : "";
     return cmd(`${proxystr}${u.arrayToString(command, " ")}`);
   });
