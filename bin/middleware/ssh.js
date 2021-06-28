a40c1089c0a9ccbef51e20f6c861ed461a96a8ae
truth1984 <zhengyao365@163.com>
2021-06-28 09:47:07 +0800

    add ssh transfer

--- bin/middleware/ssh.js
+++ bin/middleware/ssh.js
@@ -2,6 +2,7 @@ const { h, cmd, u, cu, un } = require("../head");
 h.addEntry("ssh", "use keygen to generate key pairs", {
   "-c,--connect": "connect to the target host by pattern",
   "-i,--initialize": "initialize, add to ansible list and auto connect, [$addr,$name,$desc]",
+  "-t,--transfer": "transfer public key to target machine",
   "-l,--list": "list each ip in details with ansible",
   "-p,--proxy": "proxy socks5 to [$host,$localport=15542]",
   "-r,--refresh": "refresh keygen token",
@@ -13,6 +14,7 @@ h.addEntry("ssh", "use keygen to generate key pairs", {
   .addLink(
     { _: 0, args: "c", kwargs: "connect" },
     { args: "i", kwargs: "initialize" },
+    { args: "t", kwargs: "transfer" },
     { args: "l", kwargs: "list" },
     { args: "p", kwargs: "proxy" },
     { args: "r", kwargs: "refresh" },
@@ -22,6 +24,7 @@ h.addEntry("ssh", "use keygen to generate key pairs", {
     let args = argv.args;
     let connect = args.c;
     let initialize = args.i;
+    let transfer = args.t;
     let list = args.l;
     let proxy = args.p;
     let refresh = args.r;
@@ -40,6 +43,15 @@ h.addEntry("ssh", "use keygen to generate key pairs", {
       if (status > 0) return cu.cmderr("ssh-copy-id status > 0, abort", "ssh");
       cmd(`u quick ${name} -c "ssh -p ${port} ${user}@${addr}"`);
       cmd(`u ansible -a ${addr} ${name} '${desc}'`);
+    }
+
+    if (transfer) {
+      let addrlike = transfer[0];
+      cmd(`if ! [ -f $HOME/.ssh/id_rsa ]; then ssh-keygen -t rsa -b 4096; fi;`);
+
+      let { user, addr, port } = cu.sshGrep(addrlike);
+      let status = cmd(`ssh-copy-id -i ~/.ssh/id_rsa.pub -p ${port} ${user}@${addr}`, 0, 1, 1).status;
+      if (status > 0) return cu.cmderr("ssh-copy-id status > 0, abort", "ssh");
     }
 
     if (refresh) return cmd("ssh-keygen -t rsa -b 4096");
