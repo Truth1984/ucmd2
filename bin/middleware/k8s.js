9215805dcee5bc1a9e66ae4cec73823cc2fa68a0
truth1984 <zhengyao365@163.com>
2021-07-12 17:58:09 +0800

    minor changes

--- bin/middleware/k8s.js
+++ bin/middleware/k8s.js
@@ -1,9 +1,15 @@
 const { h, cmd, u } = require("../head");
-h.addEntry("k8s", "k8s common operations", { "-c,--convert": "convert k8s.gcr.io url and save it to local image" })
-  .addLink({ args: "c", kwargs: "convert" })
+h.addEntry(
+  "k8s",
+  "k8s common operations",
+  { "-c,--convert": "convert k8s.gcr.io url and save it to local image" },
+  { "-p,--pod": "display pod lists with namespace" }
+)
+  .addLink({ args: "c", kwargs: "convert" }, { $: 0, args: "p", kwargs: "pod" })
   .addAction((argv) => {
     let args = argv.args;
     let convert = args.c;
+    let pod = args.p;
 
     if (convert) {
       let conv = (name, ver) =>
@@ -17,9 +23,13 @@ h.addEntry("k8s", "k8s common operations", { "-c,--convert": "convert k8s.gcr.io
       conv("kube-controller-manager", cver);
       conv("kube-scheduler", cver);
       conv("kube-proxy", cver);
-      conv("etcd", "3.4.9");
+      conv("etcd", "3.4.13-0");
       conv("pause", "3.4.1");
       // coredns should not be used on master node
-      cmd(`sudo docker pull coredns/coredns`);
+      cmd(
+        `sudo docker pull coredns/coredns:1.8.0 && sudo docker tag coredns/coredns:1.8.0 k8s.gcr.io/coredns/coredns:v1.8.0 && sudo docker rmi coredns/coredns:1.8.0`
+      );
     }
+
+    if (pod) return cmd(`sudo kubectl get po ${u.equal(pod, []) ? "-A" : pod[0]}`);
   });
