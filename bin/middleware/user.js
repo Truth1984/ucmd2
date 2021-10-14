feb71b5133979d1de1e60105f86cdc255ff1d37f
truth1984 <zhengyao365@163.com>
2021-10-14 10:45:52 +0800

    add user remove

--- bin/middleware/user.js
+++ bin/middleware/user.js
@@ -5,19 +5,25 @@ h.addEntry("user", "user related operation", {
   "-G,--groupadd": "add group, define [$groupname, $?groupid], groupid starting from 1000",
   "-a,--add": "add user, define [$username,$?password,$?groupname,$?groupid]",
   "-A,--adduser": "add user to group, define [$user,$group]",
+  "-r,--remove": "remove user from the group, define [$user,$group]",
+  "-R,--removeuser": "remove user from system",
 })
   .addLink(
     { args: "g", kwargs: "group" },
     { args: "G", kwargs: "groupadd" },
     { args: "a", kwargs: "add" },
-    { args: "A", kwargs: "adduser" }
+    { args: "A", kwargs: "adduser" },
+    { args: "r", kwargs: "remove" },
+    { args: "R", kwargs: "removeuser" }
   )
   .addAction((argv) => {
     let args = argv.args;
     let group = args.g;
     let groupadd = args.G;
     let add = args.a;
-    let adduser = args.a;
+    let adduser = args.A;
+    let remove = args.r;
+    let removeuser = args.R;
 
     if (group) {
       if (u.len(group) > 0) return cmd(`sudo getent group | grep ${group[0]}`);
@@ -53,5 +59,18 @@ h.addEntry("user", "user related operation", {
 
     if (adduser) {
       userToGroup(adduser[0], adduser[1]);
+    }
+
+    if (remove) {
+      let username = remove[0];
+      let group = remove[1];
+      if (!group) return cu.cmderr("username or group not defined", "user --remove");
+      return cmd(`sudo deluser ${username} ${group}`);
+    }
+
+    if (removeuser) {
+      let username = removeuser[0];
+      if (!username) return cu.cmderr("username not defined", "user --removeuser");
+      return cmd(`sudo userdel -r ${username}`);
     }
   });
