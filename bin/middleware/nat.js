45a05039c434107186b25c11d1b76743a5f14d67
truth1984 <zhengyao365@163.com>
2021-11-05 13:01:32 +0800

    add connect

--- bin/middleware/nat.js
+++ bin/middleware/nat.js
@@ -1,6 +1,8 @@
 const { h, cmd, u, cu, un } = require("../head");
 h.addEntry("nat", "nat traversal, --setup on client", {
-  "-c,--connect": "connect to the target host by pattern",
+  "-c,--connect":
+    "connect to the target host, define [$targetHost, $targetPort, $localPort, $localHost=localhost(can be local ip)]\n" +
+    "\t\t\tforward request from localHost:localPort to targetHost:targetPort\n",
   "-S,--serversetup": "server config",
   "-C,--clientsetup": "client config",
 })
@@ -16,6 +18,30 @@ h.addEntry("nat", "nat traversal, --setup on client", {
     let clientsetup = args.C;
 
     let osChecker = (name) => cmd(`u os ${name}`, 0, 1) == "true";
+
+    if (connect) {
+      let targetHost = connect[0];
+      let targetPort = connect[1];
+      let localPort = connect[2];
+      let localHost = connect[3];
+      if (!targetHost || !targetPort || !localPort)
+        return cu.cmderr("$targetHostPattern, $targetPort, $localPort, $localHost? undefined", "u nat --connect");
+      if (!localHost) localHost = "host.docker.internal";
+
+      let users = un.ansibleUserList(targetHost);
+      let target = u.len(users) > 1 ? await cu.multiSelect(users) : users[0];
+      let invdata = un.ansibleInventoryData(target);
+
+      let remotePort = invdata.ansible_port;
+      let remoteUser = "ussh";
+      let remoteHost = invdata.addr ? invdata.addr : target;
+
+      cmd("if ! sudo docker ps | grep -q autoheal ; then echo -- autoheal not deployed --; fi;");
+
+      return cmd(
+        `sudo docker run -d --name=${targetHost}sshNat${localPort} -v ${process.env.HOME}/.ssh:/root/.ssh --health-cmd="nc -z -v -w 3 ${remoteHost} ${targetPort} &> /dev/null && echo 'online' || exit 1" --health-interval=30s --health-timeout=30s -e GATEWAY_PORTS=true -e SSH_ENABLE_ROOT=true -e SSH_ENABLE_PASSWORD_AUTH=true -e SSH_ENABLE_ROOT_PASSWORD_AUTH=true --add-host=host.docker.internal:host-gateway --restart=always panubo/sshd:1.3.0 ssh -i /root/.ssh/id_rsa_ussh -N -R ${targetPort}:${localHost}:${localPort} -p ${remotePort} ${remoteUser}@${remoteHost}`
+      );
+    }
 
     if (serversetup) {
       if (un.cmdCheckStatus(`id ussh`)) {
