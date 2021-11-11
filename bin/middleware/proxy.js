ddc0b5b9414d102e22a77d5430f6db3df7853985
truth1984 <zhengyao365@163.com>
2021-11-11 08:58:44 +0800

    add noproxy

--- bin/middleware/proxy.js
+++ bin/middleware/proxy.js
@@ -1,13 +1,20 @@
 const { h, cmd, u } = require("../head");
 h.addEntry("proxy", "proxy the command", {
   "[0],-c,--command": "command to use proxy",
+  "-n,--noproxy": "command not to use proxy",
   "-g,--generate": "generate proxy export string",
   "-s,--string": "generate string for cmdline",
 })
-  .addLink({ _: 0, args: "c", kwargs: "command" }, { args: "g", kwargs: "generate" }, { args: "s", kwargs: "string" })
+  .addLink(
+    { _: 0, args: "c", kwargs: "command" },
+    { args: "n", kwargs: "noproxy" },
+    { args: "g", kwargs: "generate" },
+    { args: "s", kwargs: "string" }
+  )
   .addAction((argv) => {
     let args = argv.args;
     let command = args.c;
+    let noproxy = args.n;
     let generate = args.g;
     let string = args.s;
 
@@ -25,6 +32,12 @@ h.addEntry("proxy", "proxy the command", {
     if (string) {
       let url = string[0];
       return console.log(`https_proxy=${url} http_proxy=${url} HTTPS_PROXY=${url} HTTP_PROXY=${url}`);
+    }
+
+    if (noproxy) {
+      let noproxystr = "";
+      for (let i of checkKey) noproxystr += `${i}="" `;
+      return cmd(`${noproxystr}${u.arrayToString(u.arrayAdd(command, noproxy), " ")}`);
     }
 
     for (let i of checkKey) proxystr += env[i] ? i + "=" + env[i] + " " : "";
